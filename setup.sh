5c57e1074d8136b1c113f8526c332d8166026a44
Dustin Saunders <dsaunders2414@gmail.com>
2018-11-13 06:50:11 -0500

    Added google-chrome, spotify, rpmfusion media groups.

--- setup.sh
+++ setup.sh
@@ -2,14 +2,20 @@
 
 echo 'Default Installs'
 sudo dnf update -y
-sudo dnf install -y dnf-plugins-core
+sudo dnf install -y dnf-plugins-core fedora-workstation-repositories
 sudo dnf config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo
+sudo dnf config-manager --set-enabled google-chrome
 sudo dnf install -y https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
-sudo dnf install -y htop git maven java nodejs vim docker-ce docker-compose xorg-x11-drv-nvidia akmod-nvidia
+sudo dnf install -y htop git maven java nodejs vim docker-ce docker-compose xorg-x11-drv-nvidia akmod-nvidia chromium google-chrome-stable snapd
+sudo dnf groupupdate -y core multimedia sound-and-video
 sudo dnf update -y
 
 echo 'Installing Yarn'
 sudo npm install -g yarn
+
+echo 'Installing Spotify'
+sudo snap install spotify
+sudo snap refresh
 
 echo 'Setup Docker Group'
 sudo groupadd docker
